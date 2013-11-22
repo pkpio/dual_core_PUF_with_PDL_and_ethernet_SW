b85a3319f04af4278adb1773cac7c8c8f6fca131
Praveen Kumar Pendyala <praveendath92@gmail.com>
2013-11-22 11:34:44 +0530

    Final testing code with no batchOp support
    Final code to test the working hardware end of the code but there is
    support for doing batch operations just yet.

--- SW_Example/eth_SW_Example.cpp
+++ SW_Example/eth_SW_Example.cpp
@@ -3,26 +3,17 @@
 // Original Author: Ken Eguro and Sandro Forin
 // Includes code written by 2009 MSR intern Rene Mueller.
 //
-// Modified by : Praveen Kumar Pendyala
-// Created  : 05/27/13 
-// Modified : 06/03/13
+// Modified by	: Praveen Kumar Pendyala
+// Created		: 11/20/13 
+// Modified		: 11/22/13
 //
 // Description:
-// Send 256 bits of data.
-//	0-127 correspond to challenge A
-//	128-255 correspnd to challenge B
+// Send 128 bits of config data and 2 32-bit operands A, B as parameters
+// Receive 16-bit response through data read
 //
 //	Bugs
-//	Due to a bug in the hardware code responses actually start from memory address 01 and till memory addresss 32. So we read back 1 bit more to adjust for the offset.
-
-//Enter operands here
-//32-bit operands integer
-//Hex value is AAAA AAAA
-//Binary equivalent will be 101010101010.....101010 32-bits
-#define A 2863311530
-#define B 2863311530
-#define WRITE_SIZE 1024	// 16(# config bytes per bit) * 64 (total # in both cores combined)
-#define READ_SIZE 4		// 32-bit results is read back as 4 8-bit values
+//	Due to a bug in the hardware code, responses actually start from memory address 1 and extend till memory addresss 3.
+//	So we read back 1 bit more to adjust for the offset.
 
 #include <windows.h>
 #include <WinIoctl.h>
@@ -57,27 +48,28 @@ void error(string inErr){
 int main(int argc, char* argv[]){
 	int i=0;
 
-	//Operands
-	int OpA = A;
-	int OpB = B;
-
-	//Configuration bits for each bit in each core
-	int core0[32][16];
-	int core1[32][16];
-
-	//Assign configuration bits here. This is temporary. Will be read from file later.
-	//core0[0][]
-	
-	int challengeA[16] = {17,1,2,3,4,5,6,7,8,9,10,11,12,13,1,15};
-	int challengeB[16] = {15,14,13,12,11,10,9,8,7,6,5,4,3,2,1,19};	
+	//Challenges as integer arrays
+	int configTop[16]	 = {0,2,0,0,0,0,0,0};
+	int configBottom[16] = {0,0,0,0,0,0,255,255};
 
 	//Test harness variables
 	ETH_SIRC *SIRC_P;
 	uint8_t FPGA_ID[6];
 	bool FPGA_ID_DEF = false;
 
-	uint32_t numOps = 32;
+	uint32_t A = 0xffffffff;//operand A
+	uint32_t B = 0x00000000;//operand B
+	
+	uint32_t numOpsWrite = 16;	//write Length;
+	uint32_t numOpsRead = 2;	//read Length;
 	uint32_t numOpsReturned;
+
+	cout<<endl<<endl;
+
+/************************************************ Start of general SIRC_SW code ************************************************************************
+************************************************* Do NOT have to modify this part of code for any case of SIRC_SW use **********************************
+********** NOTE: For my convienence I commented out all print statements in the below portion of code. Uncomment to debug any issue ********************/
+
 	uint32_t tempInt;
     uint32_t driverVersion = 0;
 
@@ -93,9 +85,6 @@ int main(int argc, char* argv[]){
 
 	//Speed testing variables
 	DWORD start, end;
-
-	//Dummy variable for user action
-	char pressAnyKey;
 
 	std::ostringstream tempStream;
 
@@ -153,7 +142,7 @@ int main(int argc, char* argv[]){
 		}
 	} 
 	if(!FPGA_ID_DEF){
-		cout << "****USING DEFAULT MAC ADDRESS - AA:AA:AA:AA:AA:AA" << endl;
+		//cout << "****USING DEFAULT MAC ADDRESS - AA:AA:AA:AA:AA:AA" << endl;
 		FPGA_ID[0] = 0xAA;
 		FPGA_ID[1] = 0xAA;
 		FPGA_ID[2] = 0xAA;
@@ -175,26 +164,7 @@ int main(int argc, char* argv[]){
 		error(tempStream.str());
 	}
 
-	
-	//Reading challenges from challege.txt
-	string challengeVector;
-	ifstream challengesFile ("challenges.txt");
-	if (challengesFile.is_open()){
-		cout << endl << "Found challenges file. Attempting to read challenges from file..." << endl;
-		while (challengesFile.good() ){
-			getline (challengesFile,challengeVector);
-			if(i==0) cout << "Line 1 is : " << challengeVector << endl;
-			if(i==1) cout << "Line 2 is : " << challengeVector << endl;
-			i++;
-		}
-		challengesFile.close();
-	} else{
-		cout << endl << "Unable to open file challenges file. Assigning default challenges.." << endl;
-		//Default values will be used
-	}
-
-
-    //Get runtime parameters, for what we wont change.
+    //Get runtime parameters, for what we won't change.
     SIRC::PARAMETERS params;
     if (!SIRC_P->getParameters(&params,sizeof(params))){
 		tempStream << "Cannot getParameters from SIRC interface, code " << (int) SIRC_P->getLastError();
@@ -216,93 +186,108 @@ int main(int argc, char* argv[]){
     }
 
     //Fill up the input buffer
-    if (numOps == 0){
-       numOps = min(params.maxInputDataBytes, params.maxOutputDataBytes);
+    if (numOpsWrite == 0){
+       numOpsWrite = min(params.maxInputDataBytes, params.maxOutputDataBytes);
 	}
-	else if(numOps > params.maxInputDataBytes || numOps > params.maxOutputDataBytes){
+	else if(numOpsWrite > params.maxInputDataBytes || numOpsWrite > params.maxOutputDataBytes){
 		tempStream << "Invalid number of operations defined, must be less than or equal to " 
 			<< (int)  min(params.maxInputDataBytes, params.maxOutputDataBytes);
 		error(tempStream.str());
 	}
 
-	cout << endl << endl << "Doing a soft rest before proceeding..." << endl;
+	//cout << endl << endl << "Doing a soft rest before proceeding..." << endl;
     LogIt(LOGIT_TIME_MARKER);
 	if(!SIRC_P->sendReset()){
 		tempStream << "Reset failed with code " << (int) SIRC_P->getLastError();
 		error(tempStream.str());
 	}
-	cout << "Soft reset passed !" << endl << endl;
+	//cout << "Soft reset passed !" << endl << endl;
+	cout<<endl;
+
+
+/************************************************ End of general SIRC_SW code ************************************************************************
+************************************************* Do NOT have to modify this part of code for any case of SIRC_SW use ********************************/ 
+
+
+
+
+
+
+/************************************************ Start of user end of code ************************************************************************
+************************************************* All required changes to be done here  ************************************************************/ 
 
 	//This example circuit takes a few parameters:
-	//	Param Register 0 - number of input bytes
-	//	Param Register 1 - 32-bit multipler
-	//	Input buffer - N bytes to be multipled
-	//	Expected output - N bytes equal to (input values * multipler) % 256
-	cout << "Data sending phase started..." << endl;
+	//	Param Register 0 - Operand A
+	//	Param Register 1 - Operand B
+	//	Input buffer	 - 128-configuration bits. 64 for top line, 64 for bottom line.
+	//	Output			 - 16-bit response from the PUF
 
-	inputValues = (uint8_t *) malloc(sizeof(uint8_t) * numOps);
+	//cout << "Data sending phase started..." << endl;
+
+	inputValues = (uint8_t *) malloc(sizeof(uint8_t) * numOpsWrite);
 	assert(inputValues);
-	outputValues = (uint8_t *) malloc(sizeof(uint8_t) * (numOps+1));
+	outputValues = (uint8_t *) malloc(sizeof(uint8_t) * (numOpsRead+1));
 	assert(outputValues);
 
-	//Set input values to challengeA followed by challengeB
-	cout<<"Reading in challeneges to inputvalue"<<endl;
+	//Set input values to configuration bits
+	//cout<<"Reading in configuration bits to inputvalue"<<endl;
 	
-	cout<<"Challenge A is : ";
-	for(int i = 0; i < 16; i++){
-		inputValues[i] = challengeA[i];
+	cout<<"Configuration for TOP line:    ";
+	for(int i = 0; i < 8; i++){
+		inputValues[i] = configTop[i];
 		cout<<(int)inputValues[i]<<", ";
 	}
-	
-	cout<<endl<<"Challenge B is : ";
-	for(int i = 0; i < 16; i++){
-		inputValues[i+16] = challengeB[i];
-		cout<<(int)inputValues[i+16]<<", ";
+	cout<<endl<<"Configuration for BOTTOM line: ";
+	for(int i = 0; i < 8; i++){
+		inputValues[i+8] = configBottom[i];
+		cout<<(int)inputValues[i+8]<<", ";
 	}
+	cout<<endl;
 	
-	cout<<endl<<"End of inputs. Data ready."<<endl<<endl;
+	//cout<<endl<<"End of inputs. Data ready."<<endl<<endl;
 
 
     LogIt(LOGIT_TIME_MARKER);
 	start = GetTickCount();
-	//Set parameter register 0 to the number of operations
-	if(!SIRC_P->sendParamRegisterWrite(0, numOps)){
+	//Set parameter register 0 to the operand A
+	if(!SIRC_P->sendParamRegisterWrite(0, A)){
 		tempStream << "Parameter register write failed with code " << (int) SIRC_P->getLastError();
 		error(tempStream.str());
 	}
-	//Set parameter register 1 to a multiplier of 3
-	if(!SIRC_P->sendParamRegisterWrite(1, 3)){
+	//Set parameter register 1 to the operand B
+	if(!SIRC_P->sendParamRegisterWrite(1, B)){
 		tempStream << "Parameter register write failed with code " << (int) SIRC_P->getLastError();
 		error(tempStream.str());
 	}
 
 	//Next, send the input data
 	//Start writing at address 0
-	cout<<"Writing inputs to FPGA..."<<endl;
-	if(!SIRC_P->sendWrite(0, numOps, inputValues)){
+
+	//cout<<"Writing inputs to FPGA..."<<endl;
+	if(!SIRC_P->sendWrite(0, numOpsWrite, inputValues)){
 		tempStream << "Write to FPGA failed with code " << (int) SIRC_P->getLastError();
 		error(tempStream.str());
 	} else{
-		cout<<"Write success !"<<endl<<endl;
+		//cout<<"Write success !"<<endl<<endl;
 	}
 
 	//Set the run signal
-	cout<<"Issued a run signal"<<endl;
+	//cout<<"Issued a run signal"<<endl;
 	if(!SIRC_P->sendRun()){
 		tempStream << "Run command failed with code " << (int) SIRC_P->getLastError();
 		error(tempStream.str());
 	} else{
-		cout<<"Run command issue success !"<<endl<<endl;
+		//cout<<"Run command issue success !"<<endl<<endl;
 	}
 
 	//Wait up to N seconds for the execution to finish (we can compute ~500M numbers in that time)
 	if(waitTimeOut == 0){
-		cout<<"Allowed a waitTimeOut of : 10 secs"<<endl;
+		//cout<<"Allowed a waitTimeOut of : 10 secs"<<endl;
 		if(!SIRC_P->waitDone(10)){
 			tempStream << "Wait till done failed with code " << (int) SIRC_P->getLastError();
 			error(tempStream.str());
 		} else{
-			cout<<"User code exectution completed successfully !"<<endl<<endl;
+			//cout<<"User code exectution completed successfully !"<<endl<<endl;
 		}
 	}
 	else{
@@ -311,32 +296,27 @@ int main(int argc, char* argv[]){
 			tempStream << "Wait till done failed with code " << (int) SIRC_P->getLastError();
 			error(tempStream.str());
 		} else{
-			cout<<"User code exectution completed successfully !"<<endl<<endl;
+			//cout<<"User code exectution completed successfully !"<<endl<<endl;
 		}
 	}	
 
 	//Read the data back
-	cout<<"Atempting to read back responses"<<endl;
-	if(!SIRC_P->sendRead(0, (numOps+1), outputValues)){
+	//cout<<"Atempting to read back responses"<<endl;
+	if(!SIRC_P->sendRead(0, (numOpsRead+1), outputValues)){
 		tempStream << "Read from FPGA failed with code " << (int) SIRC_P->getLastError();
 		error(tempStream.str());
 	} else{
-		cout<<"Read back from memory success !"<<endl<<endl;
+		//cout<<"Read back from memory success !"<<endl<<endl;
 	}
 	end = GetTickCount();
 
 	//Verify that the values are correct
-	cout<<"The responses are : "<<endl;
-	for(int i = 1; i < (numOps+1); i++){
+	cout<<"The responses are : ";
+	for(int i = 1; i < (numOpsRead+1); i++){
 		cout<<(int)outputValues[i]<<", ";
-		//if((inputValues[i] * 3) % 256 != outputValues[i]){
-		//	tempStream << "Output #" << (int) i << " does not match expected value";
-		//	error(tempStream.str());
-		//}
 	}
-	cout<<endl<<"End of Outputs"<<endl<<endl;
-	cout << "Operation complete !" << endl;
-	cout << "\tExecuted in " << (end - start) << " ms" << endl << endl;
+	//cout<<endl<<"End of Outputs"<<endl<<endl;
+	cout << endl << "Operation complete !" << "\tExecuted in " << (end - start) << " ms" << endl << endl;
 
 
 	delete SIRC_P;
@@ -347,7 +327,5 @@ int main(int argc, char* argv[]){
     StartLog(susp);
 #endif
     PrintZeLog();
-	cout<<endl<<endl<<"Enter any value and press enter to exit";
-	cin>>pressAnyKey;
 	return 0;
 }
